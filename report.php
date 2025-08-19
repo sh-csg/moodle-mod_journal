fe996ffd9641b0c1a50f0f0870eac8e4a1e157cc
Stefan Hanauska <stefan.hanauska@csg-in.de>
2025-08-19 13:26:17 +0200

    Coding style

--- report.php
+++ report.php
@@ -28,7 +28,7 @@ require_once('lib.php');
 $id = required_param('id', PARAM_INT);   // Course module.
 $sortby = optional_param('sortby', 'dateasc', PARAM_ALPHA);
 
-$valid_sort_options = [
+$validsortoptions = [
     'dateasc',
     'datedesc',
     'firstnameasc',
@@ -36,7 +36,7 @@ $valid_sort_options = [
     'lastnameasc',
     'lastnamedesc',
 ];
-if (!in_array($sortby, $valid_sort_options)) {
+if (!in_array($sortby, $validsortoptions)) {
     $sortby = 'dateasc';
 }
 
@@ -93,7 +93,15 @@ if ($data = data_submitted()) {
 
         $studentrating = clean_param($vals['r'], PARAM_INT);
         $studentcomment = clean_text($vals['c']['text'], FORMAT_HTML);
-        $studentcomment = file_save_draft_area_files($vals['c']['itemid'], $context->id, 'mod_journal', 'feedback', $num, [], $studentcomment);
+        $studentcomment = file_save_draft_area_files(
+            $vals['c']['itemid'],
+            $context->id,
+            'mod_journal',
+            'feedback',
+            $num,
+            [],
+            $studentcomment
+        );
 
         if ($studentrating != $entry->rating || $studentcomment != $entry->entrycomment) {
             $ratingchanged = $studentrating != $entry->rating;
@@ -107,7 +115,9 @@ if ($data = data_submitted()) {
                 'mailed' => 0,
             ];
             if (!$DB->update_record('journal_entries', $newentry)) {
-                echo $OUTPUT->notification(get_string('failedupdate', 'journal', $entry->userid), \core\output\notification::NOTIFY_ERROR);
+                echo $OUTPUT->notification(
+                    get_string('failedupdate', 'journal', $entry->userid), \core\output\notification::NOTIFY_ERROR
+                );
             } else {
                 $count++;
                 $entrybyuser[$entry->userid]->rating = $studentrating;
